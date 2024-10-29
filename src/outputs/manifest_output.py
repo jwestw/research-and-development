42d97455a1383526b392051e1c17f1b8b59799d5
Zorinyants <george.zorinyants@ons.gov.uk>
2024-10-29 18:56:30 +0000

    Dubfolder bug fix tested in PROD, set subfolder to .dirname()

--- src/outputs/manifest_output.py
+++ src/outputs/manifest_output.py
@@ -171,14 +171,14 @@ class Manifest:
                     File size is too big"""
             )
 
-        # Remove leading slashes from the relative file path
-        relative_file_str = str(relative_file_path)
-        while relative_file_str.startswith("/"):
-            relative_file_str = relative_file_str[1:]
+        # Remove leading slashes from the relative directory path
+        relative_dir_str = str(os.path.dirname(relative_file_path))
+        while relative_dir_str.startswith("/"):
+            relative_dir_str = relative_dir_str[1:]
 
         file_manifest = {
             "file": os.path.basename(relative_file_path),
-            "subfolder": relative_file_str,
+            "subfolder": relative_dir_str,
             "sizeBytes": file_size_bytes,
             "md5sum": self.md5sum(absolute_file_path),
             "header": column_header,
